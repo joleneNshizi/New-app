4ba0c93d624478f061c85e9288674b6be3280034
joleneNshizi <joleneshamim@gmail.com>
2023-11-20 13:54:40 +0200

    Initial commit

---
+++ src/views/Dashboard.jsx
@@ -0,0 +1,31 @@
+import PageComponent from "../components/PageComponent";
+import DashboardCard from "../components/DashboardCard.jsx";
+import { useEffect, useState } from "react";
+import axiosClient from "../axios.js";
+import TButton from "../components/core/TButton.jsx";
+import { EyeIcon, PencilIcon } from "@heroicons/react/24/outline";
+
+export default function Dashboard() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    setLoading(true);
+    axiosClient
+      .get(`/dashboard`)
+      .then((res) => {
+        setLoading(false);
+        setData(res.data);
+        return res;
+      })
+      .catch((error) => {
+        setLoading(false);
+        return error;
+      });
+  }, []);
+
+  return (
+   <div> Dashboard
+   </div>
+  );
+}
